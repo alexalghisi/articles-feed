b546a189283416bce3d21a047966a0ed468d37eb
Alghisi Alessandro <alexalghisi@gmail.com>
2022-01-31 14:04:41 +0200

    Update form design 2.0

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ const options = [
 const App = () => (<div className={style.container}>
         <div className="App">
             <button className={style.newArticleButton}>New Article</button>
-            <div className={style.menuItem}>Headline: <input className={style.menuItem}/></div>
-            <div className={style.topicContainer}>Topic: <Select options={options} placeholder=''
-                                                                 className={style.dropDown}/></div>
+            <div className={style.menuItem}>Headline: <input/></div>
+            <div className={style.menuItem}>Topic: <Select options={options} placeholder=''
+                                                           className={style.dropDown}/></div>
             <div className={style.menuItem}>D.O.P: <input/></div>
             <div className={style.title}>Article: <textarea name="Text1" cols="40" rows="5"
                                                             className={style.textArea} maxLength="200" width="200px"/>
