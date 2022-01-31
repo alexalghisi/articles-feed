a93fdd631ffd3b2b8b7809f7c3932c4c55e07fe4
Alghisi Alessandro <alexalghisi@gmail.com>
2022-01-31 14:14:38 +0200

    Add new article method

--- src/App.js
+++ src/App.js
@@ -11,19 +11,26 @@ const options = [
     {value: 'Technology', label: 'Technology'}
 ];
 
-const App = () => (<div className={style.container}>
-        <div className="App">
-            <button className={style.newArticleButton}>New Article</button>
-            <div className={style.menuItem}>Headline: <input/></div>
-            <div className={style.menuItem}>Topic: <Select options={options} placeholder=''
-                                                           className={style.dropDown}/></div>
-            <div className={style.menuItem}>D.O.P: <input/></div>
-            <div className={style.title}>Article: <textarea name="Text1" cols="40" rows="5"
-                                                            className={style.textArea} maxLength="200" width="200px"/>
-            </div>
-            <button className={style.submit}>Submit</button>
-        </div>
-    </div>
-);
+const App = () => {
 
+    const onNewArticle = React.useCallback(() => {
+        window.location.reload();
+    }, []);
+
+    return (<div className={style.container}>
+            <div className="App">
+                <button className={style.newArticleButton} onClick={onNewArticle}>New Article</button>
+                <div className={style.menuItem}>Headline: <input/></div>
+                <div className={style.menuItem}>Topic: <Select options={options} placeholder=''
+                                                               className={style.dropDown}/></div>
+                <div className={style.menuItem}>D.O.P: <input/></div>
+                <div className={style.title}>Article: <textarea name="Text1" cols="40" rows="5"
+                                                                className={style.textArea} maxLength="200"
+                                                                width="200px"/>
+                </div>
+                <button className={style.submit}>Submit</button>
+            </div>
+        </div>
+    );
+}
 export default App;
